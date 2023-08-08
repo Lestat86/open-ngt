a339345d20efaf0183941e786671505a9bd2aa1c
Alberto Ronchi <alberto.ronchi786@gmail.com>
2023-08-08 17:41:58 +0200

    First datamodel

---
+++ supabase/seed.sql
@@ -0,0 +1,11 @@
+insert into
+public.criteria (criteria_name, criteria_description)
+values
+('SIGNIFICANCE', 'Statistical significance'),
+('FEASIBILITY', 'Statistical feasibility');
+
+insert into
+public.measures (measure_name)
+values
+('DEV_STD'),
+('QR');
