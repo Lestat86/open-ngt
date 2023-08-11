4b1fa8945fe1e190d6a2330af4d7c090f5bb3762
Alberto Ronchi <alberto.ronchi786@gmail.com>
2023-08-11 18:06:40 +0200

    Added trial-related apis

---
+++ src/app/api/trial-partecipants/route.ts
@@ -0,0 +1,43 @@
+import { Database } from "@/types/database.types";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function PUT(request: Request) {
+    const { partecipants } = await request.json();
+
+    const supabase = createRouteHandlerClient<Database>({ cookies });
+    const { data, error } = await supabase
+        .from('trial_partecipant')
+        .insert(partecipants)
+        .select()
+
+
+    if (error) {
+        return new NextResponse(
+            JSON.stringify({ success: false, message: error.message }),
+            { status: 500, headers: { 'content-type': 'application/json' } }
+        )
+    }
+
+    return NextResponse.json(data);
+}
+
+export async function DELETE(request: Request) {
+    const { partecipantsToDelete } = await request.json();
+
+    const supabase = createRouteHandlerClient<Database>({ cookies });
+    const { data, error } = await supabase
+        .from('trial_partecipant')
+        .delete()
+        .in('partecipant_id', partecipantsToDelete)
+
+    if (error) {
+        return new NextResponse(
+            JSON.stringify({ success: false, message: error.message }),
+            { status: 500, headers: { 'content-type': 'application/json' } }
+        )
+    }
+
+    return NextResponse.json(data);
+}
