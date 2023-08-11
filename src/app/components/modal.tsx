41c3100220ad5be15f4557011bf62dcf7626d8cb
Alberto Ronchi <alberto.ronchi786@gmail.com>
2023-08-11 18:05:01 +0200

    Added modal component

---
+++ src/app/components/modal.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function stopEvent(event: React.MouseEvent<HTMLElement>) {
+    event.stopPropagation();
+}
+
+
+type Props = {
+    show?: boolean
+    closeFun: () => void
+    children?: React.ReactNode
+}
+
+const Modal = (props: Props) => {
+    if (!props.show) {
+        return null
+    }
+
+    return (
+        <div onClick={props.closeFun}
+            className='absolute top-0 left-0 flex justify-center items-center h-screen w-screen bg-gray-900/75 z-30'>
+            <div className='flex flex-col justify-center items-center bg-white z-40'>
+                <div onClick={stopEvent} className="flex justify-center items-center w-full h-full">
+                    {props.children}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Modal
+
