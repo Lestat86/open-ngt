6ea9425e910731458bfcb72659268fa0a3babdd8
Alberto Ronchi <alberto.ronchi786@gmail.com>
2023-08-11 18:05:27 +0200

    Added client datagrid helper component

---
+++ src/app/components/client-data-grid.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import 'react-data-grid/lib/styles.css';
+
+import React from 'react'
+import DataGrid from 'react-data-grid';
+import EmptyRowsRenderer from './client-data-grid/empty-rows-renderer';
+
+interface IDataGridColumn {
+    key: string
+    name: string
+}
+
+type Props = {
+    emptyMessage: string
+    columns: IDataGridColumn[]
+    rows: any[]
+}
+
+const ClientDataGrid = (props: Props) => {
+    const { columns, rows, emptyMessage } = props
+
+    return (
+        <DataGrid columns={columns} rows={rows} renderers={{
+            noRowsFallback: <EmptyRowsRenderer message={emptyMessage} />
+        }} />
+    )
+}
+
+export default ClientDataGrid
---
+++ src/app/components/client-data-grid/empty-rows-renderer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Props = {
+    message: string
+}
+
+const EmptyRowsRenderer = (props: Props) => {
+    return (
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            textAlign: 'center', gridColumn: '1/-1'
+        }}>
+            {props.message}
+        </div>
+    )
+}
+
+export default EmptyRowsRenderer
