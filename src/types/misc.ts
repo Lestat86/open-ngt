49bf305cfba0b1fba6c544cf75f41eda05bf885c
Alberto Ronchi <alberto.ronchi786@gmail.com>
2023-08-23 11:58:50 +0200

    Added new types

--- src/types/misc.ts
+++ src/types/misc.ts
@@ -1,4 +1,4 @@
-import { Criteria, TrialItem, TrialItemWithCriteria } from './database.types';
+import { Criteria, TrialItem, TrialItemWithCriteria, TrialItemsAnswers, TrialMeasures } from './database.types';
 
 export type IGenericColumn = {
     key: string
@@ -14,9 +14,64 @@ export interface HydratedTrialItems extends TrialItem {
     trial_item_with_criteria: HydratedTrialItemWithCriteria[] | null
 }
 
+export interface ICartesianPoints {
+    x: number
+    y: number
+}
+
 export interface IChartDataset {
     label: string
     data: number[]
     borderColor: string
     backgroundColor: string
 }
+
+export interface ITrialAnswerWithCriteriaAndText extends TrialItemsAnswers {
+    trial_item: {
+        trial_id: string
+        item_text: string
+    } | null
+    criteria: {
+        criteria_name: string
+    } | null
+}
+
+export interface ICriteriaMap {
+    [criteriaId: string]: string | undefined
+}
+
+export interface IQuestionMap {
+    [questionid: string]: string | undefined
+}
+
+export interface ICriteriaTurn {
+  [criteria:number]: {
+    [turn:number]: IChartDataset
+  }
+}
+
+export interface IParsedAnswer {
+    [question: number]: ICriteriaTurn
+}
+
+export interface ITrialMeasureWithName extends TrialMeasures {
+    measures: {
+      measure_name: string
+    } | null
+  }
+
+export interface IItemStat {
+    stdev: number
+    stdevOk: boolean
+    mean: number
+    iqr: number
+    iqrOk: boolean
+}
+
+export interface ICriteriaTurnStats {
+      [criteria: number]: IItemStat
+  }
+
+export interface IItemSummary {
+      [question: number]: ICriteriaTurnStats
+  }
